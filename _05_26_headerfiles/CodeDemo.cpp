e8eed4600ba4e997affd1e55ad899e9ce85b3bb5
BACHAN SHYAM <bachanshyam999@gmail.com>
2024-05-26 17:45:49 +0530

    Initial Commit: First  commit for header file execution

---
+++ _05_26_headerfiles/CodeDemo.cpp
@@ -0,0 +1,15 @@
+// Learning C++ 
+// Exercise 03_05
+// Using Several Source Files, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+#include "cow.h"
+
+int main(void){
+    cow my_cow("Hildy", 7, cow_purpose::pet);
+    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
+    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    
+    return (0);
+}
