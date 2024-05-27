549e412bfdf571beb7ebc6cfbd4239033bcf0946
BACHAN SHYAM <bachanshyam999@gmail.com>
2024-05-28 03:06:18 +0530

    Pointer for dynamic memory allocation and deletion

---
+++ _05_27_pointers/CodeDemo.cpp
@@ -0,0 +1,17 @@
+// Learning C++ 
+// Exercise 03_05
+// Using Several Source Files, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+#include "cow.h"
+
+int main(void){
+    cow *my_cow;
+    my_cow = new cow("Hildy", 7, cow_purpose::pet);
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
+    delete my_cow;
+
+    return (0);
+}
